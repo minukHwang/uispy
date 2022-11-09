d5d0fa7ca2056c3f5e5106af9ccbf3bc7573c346
Minuk Hwang <minukhwang@Minukui-MacBookPro.local>
2022-11-09 18:13:00 +0900

    main-page-add-geo

--- js/main.js
+++ js/main.js
@@ -8,39 +8,99 @@ const canvas = document.querySelector(".canvas");
 //const context = canvas.getContext("2d");
 const pixelRatio = window.devicePixelRatio;
 
+let mouseNum = 0;
+
+let originWidth = originX * pixelRatio;
+let origintHeight = originY * pixelRatio;
+
+let colors = ["#0094FF", "#FFD600", "#04CB00"];
+
 // module aliases
 const Engine = Matter.Engine,
   Render = Matter.Render,
   Runner = Matter.Runner,
   Bodies = Matter.Bodies,
-  Composite = Matter.Composite;
+  Composite = Matter.Composite,
+  Vertices = Matter.Vertices,
+  Events = Matter.Events,
+  Svg = Matter.Svg;
 
 // create runner
 const runner = Runner.create();
 // create an engine
 const engine = Engine.create();
-
+engine.world.gravity.y = 2;
 // create a renderer
+
+// svg import
+const starPath = document.querySelector(".star-path");
+
+console.log(starPath);
+
+const starVertices = Bodies.fromVertices(
+  (originX * pixelRatio) / 2 - 100,
+  0,
+  Vertices.scale(Svg.pathToVertices(starPath), 2, 2),
+  {
+    render: {
+      fillStyle: "#FFD600",
+    },
+  },
+  true
+);
+
+console.log(starVertices);
 
 const render = Render.create({
   canvas: canvas,
   element: document.body,
   engine: engine,
   options: {
-    background: "e05406",
+    background: "transparent",
     wireframes: false,
   },
 });
 
-console.log(render);
-
 // create two boxes and a ground
 const boxA = Bodies.rectangle(
-  (window.innerWidth * pixelRatio) / 2,
+  (originX * pixelRatio) / 2,
   0,
-  500,
-  500
+  originWidth * 0.1,
+  originWidth * 0.1,
+  {
+    render: {
+      fillStyle: "#0094FF",
+    },
+    chamfer: { radius: 10 },
+  }
 );
+
+const triA = Bodies.polygon(
+  (originX * pixelRatio) / 2 + 30,
+  0,
+  3,
+  originWidth * 0.1,
+  {
+    render: {
+      fillStyle: "#04CB00",
+    },
+    chamfer: { radius: 10 },
+  }
+);
+
+const hexA = Bodies.polygon(
+  (originX * pixelRatio) / 2 + 30,
+  0,
+  7,
+  originWidth * 0.1,
+  {
+    render: {
+      fillStyle: "#FFD600",
+    },
+    chamfer: { radius: 10 },
+  }
+);
+
 const ground = Bodies.rectangle(
   0,
   30 + window.innerHeight,
@@ -68,9 +128,55 @@ const init = () => {
     window.innerWidth / originX,
     window.innerWidth / originX
   );
-  console.log(boxA);
   originX = window.innerWidth;
   originY = window.innerHeight;
+};
+
+const addGeometry = (e) => {
+  if (mouseNum == 1) {
+    const randomWidth = (originWidth * (Math.random() / 2 + 0.5)) / 10;
+    const starWidth = 2 * (Math.random() / 2 + 0.5);
+    const box = Bodies.rectangle(
+      e.mouse.mousedownPosition.x,
+      e.mouse.mousedownPosition.y,
+      randomWidth,
+      randomWidth,
+      {
+        render: {
+          fillStyle: colors[Math.floor(Math.random() * 3)],
+        },
+        chamfer: { radius: 10 },
+      }
+    );
+
+    const tri = Bodies.polygon(
+      e.mouse.mousedownPosition.x,
+      e.mouse.mousedownPosition.y,
+      3,
+      (originWidth * (Math.random() / 2 + 0.5)) / 10,
+      {
+        render: {
+          fillStyle: colors[Math.floor(Math.random() * 3)],
+        },
+        chamfer: { radius: 10 },
+      }
+    );
+
+    const star = Bodies.fromVertices(
+      e.mouse.mousedownPosition.x + 100,
+      e.mouse.mousedownPosition.y + 100,
+      Vertices.scale(Svg.pathToVertices(starPath), starWidth, starWidth),
+      {
+        render: {
+          fillStyle: colors[Math.floor(Math.random() * 3)],
+        },
+      },
+      true
+    );
+
+    // 공간에 추가합니다.
+    Composite.add(engine.world, [box, tri, star]);
+  }
 };
 
 // create mouse
@@ -84,9 +190,52 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
 
 render.mouse = mouse;
 
+Events.on(mouseConstraint, "mousedown", (e) => {
+  console.log(2)
+  mouseNum = 2;
+  addGeometry(e);
+});
+
+Events.on(mouseConstraint, "mouseup", (e) => {
+  console.log(1)
+  if(mouseNum == 3){
+    mouseNum = 3
+  } else {
+    mouseNum = 1;
+  }
+  addGeometry(e);
+});
+
+Events.on(mouseConstraint, "startdrag", (e) => {
+  console.log(3)
+  mouseNum = 3;
+  addGeometry(e);
+});
+
+Events.on(mouseConstraint, "enddrag", (e) => {
+  console.log(3)
+  mouseNum = 3;
+  addGeometry(e);
+});
+
+
+// render.canvas.addEventListener(
+//   "click",
+//   (e) => {
+//     console
+//   },
+//   false
+// );
+
 init();
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, ground, mouseConstraint]);
+Composite.add(engine.world, [
+  boxA,
+  triA,
+  starVertices,
+  ground,
+  mouseConstraint,
+]);
 // run the renderer
 Render.run(render);
 
